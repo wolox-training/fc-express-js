29c571ea02b25687d2d32fdce8c078d147b7ca54
Franco Coronel <franco.coronel@wolox.com.ar>
2018-02-27 11:49:33 -0300

    user-Model

---
+++ app/models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define(
+    'User',
+    {
+      id: DataTypes.INT,
+      name: DataTypes.STRING,
+      surname: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+    },
+    {
+      classMethods: {
+        associate(models) {
+          // associations can be defined here
+        }
+      }
+    }
+  );
+  return User;
+};
