bc580016c96adc88435eeaf7ec4baf1de5027ef2
Franco Coronel <franco.coronel@wolox.com.ar>
2018-02-28 18:15:24 -0300

    Sign-Up

---
+++ app/controllers/usersController.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const errors = require('../errors'),
+  logger = require('../logger'),
+  User = require('../models').User;
+
+exports.create = (req, res, next) => {
+  const saltRounds = 10;
+
+  const userParams = req.body
+    ? {
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        password: req.body.password
+      }
+    : {};
+
+  User.create(userParams)
+    .then(user => {
+      logger.info(`The user ${user.name} ${user.surname} was created succesfully`);
+      res.status(201).send({
+        name: user.name,
+        surname: user.surname,
+        email: user.email
+      });
+      res.end();
+    })
+    .catch(err => {
+      if (err.errors) {
+        logger.error(err.message);
+        res.status(422).send(err.message);
+      } else {
+        logger.error(errors.databaseError(err));
+      }
+    });
+};
