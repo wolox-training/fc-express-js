252a935d828d53d66e37f21f6b4fc7321cb9828b
Franco Coronel <franco.coronel@wolox.com.ar>
2018-03-01 09:46:12 -0300

    Corrections

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define(
     'User',
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,3 @@
-// const controller = require('./controllers/controller');
-
 exports.init = app => {
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
